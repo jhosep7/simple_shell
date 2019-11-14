2491bbec9f6ad0bf3a93f45c011cfa249813da09
MAZTRO <designe.mallet@gmail.com>
2019-11-14 16:38:25 -0500

    the first step

---
+++ holberton.h
@@ -0,0 +1,15 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+hsh_read_line();
+hsh_split_line(line);
+hsh_execute(args);
+
+#endif
